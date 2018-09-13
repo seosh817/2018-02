3e5429218b4842c631c3377b42f0783edd0cf7e5
slflf <slflf@DESKTOP-MKNRN15>
2018-09-13 22:22:03 +0900

    sec

--- Test/src/test1/WonToDollar.java
+++ Test/src/test1/WonToDollar.java
@@ -5,7 +5,7 @@ public class WonToDollar {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		System.out.print("원화를 입력하세요(단위 원)>>>");
 		System.out.print("원화를 입력하세요(단위 원)>>>");
 		double won = scanner.nextDouble();
 		System.out.printf("%.0f원은 $%.0f.%.0f입니다." ,won,won/1100,won%1100);
